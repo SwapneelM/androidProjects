51fc66063b62b3fca14b10415f368aa5e3c8b32e
SwapneelM <swapneel.mehta@gmail.com>
2016-03-19 17:42:11 +0530

    New App NewSunshine

--- Sunshine/app/src/main/java/com/example/android/sunshine/app/MainActivity.java
+++ Sunshine/app/src/main/java/com/example/android/sunshine/app/MainActivity.java
@@ -1,9 +1,12 @@
 package com.example.android.sunshine.app;
 
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.view.View;
-import android.view.LayoutInflater;
+
+import com.google.android.gms.appindexing.Action;
+import com.google.android.gms.appindexing.AppIndex;
+import com.google.android.gms.common.api.GoogleApiClient;
 
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -11,22 +14,70 @@ import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
+    /**
+     * ATTENTION: This was auto-generated to implement the App Indexing API.
+     * See https://g.co/AppIndexing/AndroidStudio for more information.
+     */
+    private GoogleApiClient client;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        client = new GoogleApiClient.Builder(this).addApi(AppIndex.API).build();
     }
 
-    String[] data = {
-                                "Mon 6/23 - Sunny - 31/17",
-                                "Tue 6/24 - Foggy - 21/8",
-                                "Wed 6/25 - Cloudy - 22/17",
-                                "Thurs 6/26 - Rainy - 18/11",
-                                "Fri 6/27 - Foggy - 21/10",
-                                "Sat 6/28 - TRAPPED IN WEATHERSTATION - 23/18",
-                               "Sun 6/29 - Sunny - 20/7"
-                                };
-                List<String> weekForecast = new ArrayList<String>(Arrays.asList(data));
-    View rootView = inflater.inflate(R.layout.fragment_main, container, false);
-    return rootView;
+    @Override
+    public void onStart() {
+        super.onStart();
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        client.connect();
+        Action viewAction = Action.newAction(
+                Action.TYPE_VIEW, // TODO: choose an action type.
+                "Main Page", // TODO: Define a title for the content shown.
+                // TODO: If you have web page content that matches this app activity's content,
+                // make sure this auto-generated web page URL is correct.
+                // Otherwise, set the URL to null.
+                Uri.parse("http://host/path"),
+                // TODO: Make sure this auto-generated app deep link URI is correct.
+                Uri.parse("android-app://com.example.android.sunshine.app/http/host/path")
+        );
+        AppIndex.AppIndexApi.start(client, viewAction);
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        Action viewAction = Action.newAction(
+                Action.TYPE_VIEW, // TODO: choose an action type.
+                "Main Page", // TODO: Define a title for the content shown.
+                // TODO: If you have web page content that matches this app activity's content,
+                // make sure this auto-generated web page URL is correct.
+                // Otherwise, set the URL to null.
+                Uri.parse("http://host/path"),
+                // TODO: Make sure this auto-generated app deep link URI is correct.
+                Uri.parse("android-app://com.example.android.sunshine.app/http/host/path")
+        );
+        AppIndex.AppIndexApi.end(client, viewAction);
+        client.disconnect();
+    }
+
+        String[] data = {
+        "Mon 6/23 - Sunny - 31/17",
+        "Tue 6/24 - Foggy - 21/8",
+        "Wed 6/25 - Cloudy - 22/17",
+        "Thurs 6/26 - Rainy - 18/11",
+        "Fri 6/27 - Foggy - 21/10",
+        "Sat 6/28 - TRAPPED IN WEATHERSTATION - 23/18",
+        "Sun 6/29 - Sunny - 20/7"
+        };
+        List<String> weekForecast = new ArrayList<>(Arrays.asList(data));
+
 }
